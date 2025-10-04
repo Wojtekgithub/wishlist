cfcece301b025dfd8f668f51cd5186e114a4b99b
Wojtek <w.maciaszek.tp22@zsme.tarnow.pl>
2025-10-04 02:02:12 +0200

    <select> works

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { WishList } from '../wishlist';
 import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
-  imports: [CommonModule],
+  imports: [CommonModule, FormsModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css',
 })
@@ -14,5 +15,28 @@ export class AppComponent {
     new WishList('wefweuyhweuif'),
     new WishList('wefwqwefwefwefeweuyhweuif'),
   ];
+  newWishText = '';
   title = 'wishlist';
+
+  visibleItems: WishList[] = this.items;
+
+  toogleItem(e: WishList) {
+    e.isComplete = !e.isComplete;
+    alert(e.wishText + ' - ' + e.isComplete);
+  }
+  addnewwish() {
+    this.items.push(new WishList(this.newWishText));
+    this.newWishText = '';
+  }
+  listFilter: string = '0';
+
+  filterChanged(value: any) {
+    if (value == 0) {
+      this.visibleItems = this.items;
+    } else if (value == 1) {
+      this.visibleItems = this.items.filter((item) => !item.isComplete);
+    } else {
+      this.visibleItems = this.items.filter((item) => item.isComplete);
+    }
+  }
 }
